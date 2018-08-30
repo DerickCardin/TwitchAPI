5a24f656f067288c4a3511b3b5ee55966e40da76
Derick Cardin <derickc50@gmail.com>
2018-08-30 20:59:54 +0000

    Final Push

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ var streamapi="https://wind-bow.glitch.me/twitch-api/streams/";
 var channelapi="https://wind-bow.glitch.me/twitch-api/channels/";
 var channels=["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-function allStreamers(streamchannel){ // it will call twitch APIs to get both streaming and channel information about that  specific channel.
+function allStreamers(streamchannel){ 
 	var logo,name,game,status,statusdesc,channel_link;
 
 	var streamchannel_url=streamapi+streamchannel+"?callback=?";
 	var channel_url=channelapi+streamchannel+"?callback=?";
 
   
-   // call streaming channels API to see if it is streaming or not and if it is then it'll display who's streaming 
+   
    
 	$.getJSON(streamchannel_url,function(data){
-		if(data.status=='404'){ /* number used when something is unidentified or not found */
+		if(data.status=='404'){ 
 			game=data.message;
 			status="offline";
 			statusdesc="";
 		}
-		else if(data.status=='422'){ // When user is not available or have their account closed 
+		else if(data.status=='422'){ 
 			game=data.message;
 			status="offline";
 			statusdesc="";
 		}
 		else{
 			data=data.stream;
-			if(data===null){ // Displays when the streamer is offline
+			if(data===null){
 				game="offline";
 				status="offline";
 				statusdesc="";
@@ -36,28 +36,26 @@ function allStreamers(streamchannel){ // it will call twitch APIs to get both st
 				statusdesc=":"+data.channel.status;
 			}
 		}
-    /*
-      Calls the channels api so that it will show the streamers info such as name, the logo etc...
-    >> this is all displayed under */
+    
     $.getJSON(channel_url,function(data){
 			name=data.display_name;
 			logo=data.logo;
     	channel_link=data.url;
-    	if(data.status=='404'){ // if the channel isn't found 
+    	if(data.status=='404'){  
     		name=streamchannel;
     		channel_link="#";
     		logo="https://openclipart.org/image/2400px/svg_to_png/211821/matt-icons_preferences-desktop-personal.png";
     	}
-    	else if(data.status=='422'){ //if the streamers channel isn't available or their accounts closed 
+    	else if(data.status=='422'){  
     		name=streamchannel;
     		channel_link="#";
     		logo="https://openclipart.org/image/2400px/svg_to_png/211821/matt-icons_preferences-desktop-personal.png";
     	}
-    	else if(logo===null){ // If the channel doesn't have logo then it will display a logo by default 
+    	else if(logo===null){  
        logo="https://openclipart.org/image/2400px/svg_to_png/211821/matt-icons_preferences-desktop-personal.png";
 			}
 
-      // prepare a row for the result in html 
+    
 			var result="\
 			<div class='row' id='"+status+"'>\
 				<div class='col-md-3 col-xs-4'>\
@@ -82,14 +80,11 @@ function allStreamers(streamchannel){ // it will call twitch APIs to get both st
 
 $(document).ready(function(){
   
-   // Calling the allStreamCall function on each and every channel
-   
+  
 	channels.forEach(function(channel){
 		allStreamers(channel);
 	});
-
-    // Once clicking on the "All of your Streamers!" button it shows all the streamers you picked or have
-   
+ 
   $('#all').click(function(){
   	var all=$('.res .row');
   	all.each(function(index){
@@ -97,9 +92,7 @@ $(document).ready(function(){
   	});
   });
 
-  
-  // Displays only the online streaming channels and hides the streamers who are offline (using the toggle and calling the Id's for both online and offline)
-   
+
   $('#online').click(function(){
   	var online=$('.res .row');
   	online.each(function(index){
@@ -113,9 +106,7 @@ $(document).ready(function(){
   	});
   });
 
-  
-  //   Once clicking on the "Who's Offline?" button it acceses the users who are offline
-   
+
   $('#offline').click(function(){
   	var offline=$('.res .row');
   	offline.each(function(index){
